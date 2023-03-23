d114096852157dfe176b1e732e9d01e3215de012
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-03-23 08:47:00 +0100

    apollo client added to context

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+// add API to Apollo Client
+const client = new ApolloClient({
+  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clfjk303n23bz01uedlua2ovd/master",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
