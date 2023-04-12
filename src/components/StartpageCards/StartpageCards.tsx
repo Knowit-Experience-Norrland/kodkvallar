38107e98d2079f64a4dc2c4fb53e64e9c501135c
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-04-12 11:38:07 +0200

    css fixes to startpage postits

--- src/components/StartpageCards/StartpageCards.tsx
+++ src/components/StartpageCards/StartpageCards.tsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import {IoMdArrowForward} from 'react-icons/io'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { IoMdArrowForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 //props from parent component
 type Props = {
-    img: string
-    title: string
-    text: string
-    link: string
-}
+  img: string;
+  title: string;
+  text: string;
+  link: string;
+};
 
-const startpageCards = ({img, title, text, link}: Props) => {
+const startpageCards = ({ img, title, text, link }: Props) => {
   return (
-    <div className='startpage-card'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <p>{text}</p>
-      <Link to={link} className='startpage-card-link'><IoMdArrowForward title="Knapp"/></Link>
-      
+    <div className="startpage-card">
+      <Link to={link} className="startpage-card-link">
+        <img src={img} alt={title} />
+        <h2>{title}</h2>
+        <p>{text}</p>
+        <IoMdArrowForward title={"Länk till " + title} className="arrow"/>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default startpageCards
+export default startpageCards;
