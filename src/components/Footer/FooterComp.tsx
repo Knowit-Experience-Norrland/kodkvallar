e2cc3cf30d2a821a594e19949c02e582a0ede6c0
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-05-17 08:55:53 +0200

    social links update

--- src/components/Footer/FooterComp.tsx
+++ src/components/Footer/FooterComp.tsx
@@ -12,19 +12,20 @@ const FooterComp = () => {
       <footer>
         <div className="footer-links">
           <div className="footer-links-socials">
-            <a href="/" target="_blank" rel="noreferrer" title={"LinkedIn"}>
+            <a href="https://www.linkedin.com/in/katarinabjuhr/" target="_blank" rel="noopener noreferrer" title={"LinkedIn"}>
               <RiLinkedinBoxLine />
             </a>
-            <a href="/" target="_blank" rel="noreferrer" title={"Instagram"}>
+            <a href="https://www.instagram.com/weareknowit/" target="_blank" rel="noopener noreferrer"  title={"Instagram"}>
+            
               <RiInstagramLine />
             </a>
-            <a href="/" target="_blank" rel="noreferrer" title={"Facebook"}>
+            <a href="https://www.facebook.com/weareknowit" target="_blank" rel="noopener noreferrer" title={"Facebook"}>
               <RiFacebookBoxLine />
             </a>
           </div>
           <div className="footer-contact">
             <p>© 2023 Knowit Experience Norrland</p>
-            <a href="mailto:info@knowit.se">info@knowit.se</a>
+            <a href="mailto:katarina.bjuhr@knowit.se">katarina.bjuhr@knowit.se</a>
             <p>Västra Norrlandsgatan 29, Umeå </p>
           </div>
           <Link to="/">
