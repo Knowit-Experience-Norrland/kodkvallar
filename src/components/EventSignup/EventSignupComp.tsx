c3a0194ec4a85475028ee46fb05d62e91630c1b8
Jessica Ejelöv <jessicaejelov@gmail.com>
2023-05-24 11:13:59 +0200

    changed form to react hook forms

--- src/components/EventSignup/EventSignupComp.tsx
+++ src/components/EventSignup/EventSignupComp.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/client";
 import React, { useState } from "react";
 import { CREATE_EVENTSIGNUP } from "../../Queries/mutations";
+import { useForm } from "react-hook-form";
 
 // recive id from EventSignupComp.tsx, send data from API and print to DOM
 interface EventSignupCompProps {
@@ -8,184 +9,117 @@ interface EventSignupCompProps {
 }
 
 const EventSignupComp = ({ slug }: EventSignupCompProps) => {
-  const [firstname, setFirstname] = useState("");
-  const [lastname, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [photoConsent, setPhotoConsent] = useState(false);
   const [message, setMessage] = useState<string | null>(null);
-  const [allergies, setAllergies] = useState("");
-  const [occupation, setOccupation] = useState("");
-
-  //error messages
-  const [firstnameError, setFirstnameError] = useState<string | null>(null);
-  const [lastnameError, setLastnameError] = useState<string | null>(null);
-  const [emailError, setEmailError] = useState<string | null>(null);
-  const [occupationError, setOccupationError] = useState<string | null>(null);
-  const [allergiesError, setAllergiesError] = useState<string | null>(null);
-
-  // Regular expressions for input validation
-  const nameRegex = /^[A-Öa-ö0-9\s,.-]+$/; // Accepts only letters and whitespace
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Basic email validation
-
-//use mutation to create event signup
+  //use mutation to create event signup
   const [createEventSignup] = useMutation(CREATE_EVENTSIGNUP);
 
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
   // handle submit
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-
-    //reset error msg
-    setFirstnameError(null);
-    setLastnameError(null);
-    setEmailError(null);
-    setOccupationError(null);
-    setAllergiesError(null);
-
-    //validate form inputs
-    let isValid = true;
-
-    if (!firstname) {
-      setFirstnameError("Fyll i förnamn");
-      isValid = false;
-    } else if (firstname.length < 2) {
-      setFirstnameError("Förnamnet måste vara minst 2 tecken");
-      isValid = false;
-    } else if (!firstname.match(nameRegex)) {
-      setFirstnameError("Förnamnet får endast innehålla bokstäver");
-      isValid = false;
+  const onSubmit = (data: any) => {
+    try {
+      data = {
+        firstName: data.eventfirstname,
+        lastName: data.eventlastname,
+        email: data.eventemail,
+        eventPageSlug: { connect: { slug: slug } },
+        photoConsent: data.photoConsent,
+        allergies: data.allergies,
+        occupation: data.occupation,
+      };
+      createEventSignup({ variables: { data } });
+      setMessage("SUCCESS");
+      reset();
+    } catch (err) {
+      setMessage("ERROR");
+      console.log(err);
     }
-    if (!lastname) {
-      setLastnameError("Fyll i efternamn");
-      isValid = false;
-    } else if (lastname.length < 2) {
-      setLastnameError("Efternamnet måste vara minst 2 tecken");
-      isValid = false;
-    } else if (!lastname.match(nameRegex)) {
-      setLastnameError("Efternamnet får endast innehålla bokstäver");
-      isValid = false;
-    }
-    if (!email) {
-      setEmailError("Fyll i e-post");
-      isValid = false;
-    } else if (!email.includes("@")) {
-      setEmailError("Ogiltig email");
-      isValid = false;
-    } else if (!email.match(emailRegex)) {
-      setEmailError("Ogiltig email");
-      isValid = false;
-    }
-    if (!occupation) {
-      setOccupationError("Fyll i utbildning/yrke");
-      isValid = false;
-    } else if (occupation.length < 2) {
-      setOccupationError("Utbildning/yrke måste vara minst 2 tecken");
-      isValid = false;
-    }
-    if (allergies.length > 0) {
-      if (!allergies.match(nameRegex)) {
-        setAllergiesError(
-          "Allergier får endast innehålla bokstäver och mellanslag"
-        );
-        isValid = false;
-      }
-    }
-
-    if (isValid) {
-      try {
-        const data = {
-          firstName: firstname,
-          lastName: lastname,
-          email: email,
-          eventPageSlug: { connect: { slug: slug } },
-          photoConsent: photoConsent,
-          allergies: allergies,
-          occupation: occupation,
-        };
-        createEventSignup({ variables: { data } });
-        setMessage("SUCCESS");
-      } catch (err) {
-        setMessage("ERROR");
-        console.log(err);
-      }
-      setFirstname("");
-      setLastname("");
-      setEmail("");
-      setPhotoConsent(false);
-      setAllergies("");
-      setOccupation("");
-    }
-  };
-
-  //   set states to values of input fields
-  const handleFirstnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFirstname(e.target.value);
-  };
-  const handleLastnameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setLastname(e.target.value);
-  };
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
-  };
-  const handlePhotoConsentChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPhotoConsent(e.target.checked);
-  };
-  const handleAllergiesChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setAllergies(e.target.value);
-  };
-  const handleOccupationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setOccupation(e.target.value);
   };
 
   return (
     <section className="event-signup">
       <div className="event-signup-container">
         <div className="form-container">
-
-          <form onSubmit={handleSubmit} name="NewsletterSignup">
-          <h2>Anmäl dig till eventet!</h2>
+          <form onSubmit={handleSubmit(onSubmit)} name="NewsletterSignup">
+            <h2>Anmäl dig till eventet!</h2>
             <div className="form-child">
-              <label htmlFor="event-firstname">
+              <label htmlFor="eventfirstname">
                 Förnamn: <span className="required">*</span>
               </label>
               <input
-                id="event-firstname"
-                name="event-firstname"
+                id="eventfirstname"
                 type="text"
                 aria-label="Förnamn"
                 className="smaller-input"
-                value={firstname}
-                onChange={handleFirstnameChange}
+                {...register("eventfirstname", {
+                  required: "Detta fält är obligatoriskt.",
+                  pattern: {
+                    value: /^[A-Za-zåäöÅÄÖ0-9\s.,"\-_\n\r]+$/, // default pattern for text input type
+                    message:
+                      "Felaktigt format. Endast bokstäver och skiljetecken tillåtna.", // default error message for text input type
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "Minst 2 tecken.",
+                  },
+                })}
               />
-              {firstnameError && <p className="error">{firstnameError}</p>}
+              {errors.eventfirstname && (
+                <p className="error">
+                  {(errors.eventfirstname as any).message}
+                </p>
+              )}
             </div>
             <div className="form-child">
-              <label htmlFor="event-lastname">
+              <label htmlFor="eventlastname">
                 Efternamn: <span className="required">*</span>
               </label>
               <input
-                id="event-lastname"
-                name="event-lastname"
+                id="eventlastname"
                 type="text"
                 aria-label="Efternamn"
                 className="smaller-input"
-                value={lastname}
-                onChange={handleLastnameChange}
+                {...register("eventlastname", {
+                  required: "Detta fält är obligatoriskt.",
+                  pattern: {
+                    value: /^[A-Za-zåäöÅÄÖ0-9\s.,"\-_\n\r]+$/, // default pattern for text input type
+                    message:
+                      "Felaktigt format. Endast bokstäver och skiljetecken tillåtna.", // default error message for text input type
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "Minst 2 tecken.",
+                  },
+                })}
               />
-              {lastnameError && <p className="error">{lastnameError}</p>}
+              {errors.eventlastname && (
+                <p className="error">{(errors.eventlastname as any).message}</p>
+              )}
             </div>
             <div className="form-child">
-              <label htmlFor="event-email">
+              <label htmlFor="eventemail">
                 Email: <span className="required">*</span>
               </label>
               <input
-                id="event-email"
-                name="event-email"
+                id="eventemail"
                 type="email"
                 aria-label="Email"
-                value={email}
-                onChange={handleEmailChange}
+                {...register("eventemail", {
+                  required: "Detta fält är obligatoriskt.",
+                  pattern: {
+                    value: /\S+@\S+\.\S+/, // default pattern for text input type
+                    message: "Felaktigt e-postformat.", // default error message for text input type
+                  },
+                })}
               />
-              {emailError && <p className="error">{emailError}</p>}
+              {errors.eventemail && (
+                <p className="error">{(errors.eventemail as any).message}</p>
+              )}
             </div>
             <div className="form-child">
               <label htmlFor="occupation">
@@ -194,13 +128,24 @@ const EventSignupComp = ({ slug }: EventSignupCompProps) => {
               </label>
               <input
                 id="occupation"
-                name="occupation"
                 type="text"
                 aria-label="Utbildning eller sysselsättning"
-                value={occupation}
-                onChange={handleOccupationChange}
+                {...register("occupation", {
+                  required: "Detta fält är obligatoriskt.",
+                  pattern: {
+                    value: /^[A-Za-zåäöÅÄÖ0-9\s.,"\-_\n\r]+$/, // default pattern for text input type
+                    message:
+                      "Felaktigt format. Endast bokstäver och skiljetecken tillåtna.", // default error message for text input type
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "Minst 2 tecken.",
+                  },
+                })}
               />
-              {occupationError && <p className="error">{occupationError}</p>}
+              {errors.occupation && (
+                <p className="error">{(errors.occupation as any).message}</p>
+              )}{" "}
             </div>
             <div className="form-child">
               <label htmlFor="allergies">
@@ -208,35 +153,48 @@ const EventSignupComp = ({ slug }: EventSignupCompProps) => {
               </label>
               <textarea
                 id="allergies"
-                name="allergies"
                 aria-label="Allergier eller annan viktig information"
                 rows={5}
                 cols={33}
-                value={allergies}
-                onChange={handleAllergiesChange}
+                {...register("allergies", {
+                  pattern: {
+                    value: /^[A-Za-zåäöÅÄÖ0-9\s.,"\-_\n\r]+$/, // default pattern for text input type
+                    message:
+                      "Felaktigt format. Endast bokstäver och skiljetecken tillåtna.", // default error message for text input type
+                  },
+                  minLength: {
+                    value: 2,
+                    message: "Minst 2 tecken.",
+                  },
+                })}
               />
-              {allergiesError && <p className="error">{allergiesError}</p>}
+              {errors.allergies && (
+                <p className="error">{(errors.allergies as any).message}</p>
+              )}{" "}
             </div>
             <div className="form-child checkbox">
               <input
                 type="checkbox"
-                id="photo-consent"
-                name="photo-consent"
-                checked={photoConsent}
+                id="photoconsent"
                 aria-label="Godkänn att vara med på bild genom att bocka i rutan"
-                onChange={handlePhotoConsentChange}
+                {...register("photoconsent")}
               />
               <label htmlFor="photo-consent">
                 Klicka i om du godkänner att vara med på bild från eventet.
               </label>
             </div>
             <div>
-              {message === "SUCCESS" &&   <>
-              <p className="green bold">Tack för din anmälan! En bekräftelse kommer på mail.</p>
-              <p className="gdpr-text">
-                Pssst! Ibland hamnar vi i skärpkorgen, så kolla där om du inte får något mail!
-              </p>
-            </>}
+              {message === "SUCCESS" && (
+                <>
+                  <p className="green bold">
+                    Tack för din anmälan! En bekräftelse kommer på mail.
+                  </p>
+                  <p className="gdpr-text">
+                    Pssst! Ibland hamnar vi i skärpkorgen, så kolla där om du
+                    inte får något mail!
+                  </p>
+                </>
+              )}
               {message === "ERROR" && (
                 <p className="error">Något gick fel. Vänligen försök igen.</p>
               )}
